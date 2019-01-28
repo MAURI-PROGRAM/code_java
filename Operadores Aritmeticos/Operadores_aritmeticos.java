d44706423c2dae14b2546ec2e5da148b625bd89b
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-01-28 13:26:08 -0500

    operadores aritmeticos

---
+++ Operadores Aritmeticos/Operadores_aritmeticos.java
@@ -0,0 +1,9 @@
+public class Operadores_aritmeticos{
+  public static void main(String args[]){
+    int num_uno = 4;
+    int num_dos = 4;
+    int resultado = 0;
+    resultado = num_uno + num_dos /2;
+    System.out.println("El resultado es: "+resultado);
+  }
+}
