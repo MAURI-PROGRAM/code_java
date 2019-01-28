a021c422709726ab86320515deb667faee31637f
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-01-28 12:59:22 -0500

    Tipos de datos en java

---
+++ Tipos de datos/Tipos_de_datos.java
@@ -0,0 +1,9 @@
+public class Tipos_de_datos{
+  public static void main(String args[]){
+    int num_uno = 2;
+    int num_dos = 5;
+    int resultado = 0;
+    resultado = num_uno + num_dos;
+    System.out.println("El valor de tu suma es: "+resultado);
+  }
+}
