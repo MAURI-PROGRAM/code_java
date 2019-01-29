92dd9944f8b41b2b3fea39708439b14edbede478
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-01-29 15:28:33 -0500

    Programacion orientado a Objeto

---
+++ Proyectos con netbeans/AreaRectangulo/src/area/Rectangulo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package area;
+
+/**
+ *
+ * @author Michael
+ */
+public class Rectangulo {
+    private int altura ,base, area;
+    public Rectangulo(int base,int altura){
+        this.base = base;
+        this.altura = altura;
+    }
+    
+    public void CalculoArea(){
+        area = base * altura;
+    }
+    public void Imprimir(){
+        CalculoArea();
+        System.out.println("El area es : "+area);
+    }
+}
+
