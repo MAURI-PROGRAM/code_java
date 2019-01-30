a38bf9c32ca2aabf0eb5e94b7a5407cab2b9160f
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-01-30 12:48:11 -0500

    palabra reservada super

---
+++ Proyectos con netbeans/PalabraSuper/src/clases/Hija.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Michael
+ */
+public class Hija extends Padre{
+    public void Saludar(){
+        super.Saludar();
+        System.out.println("Hola yo soy la hija");
+    }
+}
