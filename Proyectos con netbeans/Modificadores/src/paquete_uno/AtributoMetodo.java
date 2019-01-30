46b446094ad3624102624afc6e62aae3dbd62e05
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-01-29 20:01:37 -0500

    modificadores de acceso

---
+++ Proyectos con netbeans/Modificadores/src/paquete_uno/AtributoMetodo.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete_uno;
+
+/**
+ *
+ * @author Michael
+ */
+public class AtributoMetodo {
+    int valorUno;
+    int valorDos;
+    public String valor;
+    private int newvalor;
+    protected int oldvalor;
+    
+    void MetodoUno(){
+    }
+    public void MetodoDos(){
+    }
+    private void MetodoTres(){
+    }
+    
+    protected void MetodoCuatro(){
+        
+    }
+
+    /**
+     * @return the newvalor
+     */
+    public int getNewvalor() {
+        return newvalor;
+    }
+
+    /**
+     * @param newvalor the newvalor to set
+     */
+    public void setNewvalor(int newvalor) {
+        this.newvalor = newvalor;
+    }
+    
+}
---
+++ Proyectos con netbeans/Modificadores/src/paquete_uno/ClaseJavaUno.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete_uno;
+
+/**
+ *
+ * @author Michael
+ */
+public class ClaseJavaUno {
+    public static void main(String args[]){
+        AtributoMetodo mensajero = new AtributoMetodo();
+        mensajero.valorUno = 5;
+        mensajero.setNewvalor(12);
+        mensajero.MetodoCuatro();
+    }
+}
