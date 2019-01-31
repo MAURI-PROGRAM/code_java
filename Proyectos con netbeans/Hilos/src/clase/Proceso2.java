cf13ac52fac20782de346ddcd6297127b9b54d70
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-01-30 20:37:55 -0500

    Hilos

---
+++ Proyectos con netbeans/Hilos/src/clase/Proceso2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase;
+
+/**
+ *
+ * @author Michael
+ */
+public class Proceso2 implements Runnable{
+    @Override
+    public void run(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Proceso2");
+        }
+    }
+}
